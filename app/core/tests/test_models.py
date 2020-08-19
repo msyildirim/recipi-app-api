b2d369d16a2355c10aa5d85faf5b2bf92f5c3424
root <root@localhost.localdomain>
2020-08-19 08:44:50 +0300

    Configured postgres db.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
 
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """test creating a new user with an email is successful"""
         email = 'test@msytech.com'
@@ -11,20 +11,20 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-     
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-     
+
     def test_new_user_email_normalized(self):
         """test the email for a new user is normalized """
         email = 'test@Msytech.com'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower()) 
-  
+
     def test_new_user_invalid_email(self):
         """Test creating user with no emiail raises error"""
         with self.assertRaises(ValueError):
-           get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, 'test123')
 
 
     def test_create_new_superuser(self):
